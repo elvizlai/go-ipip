c4f895289510fb440d440a316b0f6f3bb106f295
IPIP.net <frk@ipip.net>
2018-01-23 17:38:23 +0800

    Update city.go

--- ipip/city.go
+++ ipip/city.go
@@ -113,7 +113,7 @@ func (db *CityDB) _find(s string) ([]byte, error) {
     if indexOffset == -1 || indexLength == -1 {
         return nil, fmt.Errorf("error[%s]", "find failed.")
     }
-
+    fmt.Printf("offset[%d].length[%d]\n", indexOffset, indexLength)
     var area = make([]byte, indexLength)
     indexOffset = int(db.offset) + indexOffset - 262144
     copy(area, db.all[indexOffset:indexOffset+indexLength])
